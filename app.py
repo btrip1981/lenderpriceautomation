dae588c69bacc5dedae3e998b51fb87b1ad212b6
btrip1981 <121128344+btrip1981@users.noreply.github.com>
2023-03-21 15:39:21 -0500

    update to req

--- app.py
+++ app.py
@@ -1,43 +1,7 @@
 from flask import Flask, render_template, request, redirect
 from flask_mail import Mail, Message
-import tkinter as tk
-from tkinter import ttk
+import re
 
-# BEFORE YOU RUN THIS CODE MAKE SURE YOU HAVE RUN THE GUI AND ENTERED YOUR EMAIL AND PASSWORD. THE WINDOW MAY
-# BE HIDDEN BEHIND OTHER WINDOWS. IF YOU DO NOT SEE THE WINDOW, CLICK ON THE WINDOWS ICON IN THE BOTTOM LEFT.
-
-# This function takes your credentials for your email account and saves them to a file called credentials.txt
-def submit():
-    global email, password
-    email = email_entry.get()
-    password = password_entry.get()
-
-    with open("credentials.txt", "w") as f:
-        f.write(email + "\n")
-        f.write(password + "\n")
-# DO NOT CHANGE ANY OF THIS CODE
-# this calls the library tkinter and creates a window called root which is used to create the GUI
-root = tk.Tk()
-root.title("Login")
-# DO NOT CHANGE ANY OF THIS CODE
-# GUI (graphic user interface) that gets your email to use as the sender of the email.
-email_label = ttk.Label(root, text="Email:")
-email_label.grid(column=0, row=0, padx=(20, 10), pady=(20, 5), sticky=tk.W)
-email_entry = ttk.Entry(root)
-email_entry.grid(column=1, row=0, padx=(10, 20), pady=(20, 5))
-# DO NOT CHANGE ANY OF THIS CODE
-# GUI (graphic user interface) that gets your password to use as the sender of the email.
-password_label = ttk.Label(root, text="Password:")
-password_label.grid(column=0, row=1, padx=(20, 10), pady=5, sticky=tk.W)
-password_entry = ttk.Entry(root, show="*")
-password_entry.grid(column=1, row=1, padx=(10, 20), pady=5)
-# DO NOT CHANGE ANY OF THIS CODE
-# submit button that saves your email and password to a file called credentials.txt
-submit_button = ttk.Button(root, text="Submit", command=submit)
-submit_button.grid(column=1, row=2, padx=(10, 20), pady=(5, 20))
-# end of GUI root.mainloop() calls the GUI to run
-root.mainloop()
-# call to flask to create a web app
 app = Flask(__name__)
 # DO NOT CHANGE ANY OF THIS CODE
 # function to get email credentials from the file credentials.txt
@@ -93,7 +57,7 @@ def read_qual_data():
             "min_prop_value": 100000,
             "LTV": 90,
             "credit_score": 575,
-            "season": 0,
+            "season": None,
         },
         {
             "lender_name": "Bret.Chard@carringtonms.com",
@@ -109,7 +73,7 @@ def read_qual_data():
             "min_prop_value": 100000,
             "LTV": 80,
             "credit_score": 680,
-            "season": 0,
+            "season": None,
         },
         {
             "lender_name": "alex@greenboxloans.com",
@@ -117,7 +81,7 @@ def read_qual_data():
             "min_prop_value": None,
             "LTV": 70,
             "credit_score": 680,
-            "season": 0,
+            "season": None,
         },
         {
             "lender_name": "jcalderon@homexmortgage.com",
@@ -125,12 +89,12 @@ def read_qual_data():
             "min_prop_value": 150000,
             "LTV": 80,
             "credit_score": 600,
-            "season": 0,
+            "season": None,
         },
         {
             "lender_name": "jcraft@lsmortgage.com",
             "min_loan_amt": 125000,
-            "min_prop_value": 0,
+            "min_prop_value": None,
             "LTV": 80,
             "credit_score": 620,
             "season": 3,
@@ -138,7 +102,7 @@ def read_qual_data():
         {
             "lender_name": "acureton@oaktreefunding.com",
             "min_loan_amt": 100000,
-            "min_prop_value": 0,
+            "min_prop_value": None,
             "LTV": 80,
             "credit_score": 620,
             "season": 6,
@@ -149,7 +113,7 @@ def read_qual_data():
             "min_prop_value": 83000,
             "LTV": 80,
             "credit_score": 620,
-            "season": 0,
+            "season": None,
         },
     ]
     return lender_data
@@ -167,7 +131,7 @@ def match_lenders(loan_amount, property_value, credit_score):
     matched_lenders = []
 
     for lender in read_qual_data():
-        if ltv <= lender["max_ltv"] and credit_score >= lender["min_credit_score"]:
+        if ltv <= lender["LTV"] and credit_score >= lender["credit_score"]:
             matched_lenders.append(lender)
 
     return matched_lenders
@@ -175,6 +139,8 @@ def match_lenders(loan_amount, property_value, credit_score):
 # DO NOT CHANGE ANY OF THIS CODE
 # this function processes the form data and matches it to the data in the lender_data list
 def process_form_data(form_data):
+    credit_score_clean = re.sub(r'\D', '', form_data["credit_score"])
+    credit_score = int(credit_score_clean)
     loan_amount = float(form_data["loan_amount"])
     property_value = float(form_data["property_value"])
     credit_score = int(form_data["credit_score"])
