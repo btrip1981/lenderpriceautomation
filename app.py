19346972e86bddbbd15d9c8d5860927d4987e082
btrip1981 <121128344+btrip1981@users.noreply.github.com>
2023-03-22 10:47:18 -0500

    update to req

--- app.py
+++ app.py
@@ -164,4 +164,4 @@ def send_email(matched_lenders, form_data):
 
 # DO NOT CHANGE ANY OF THIS CODE
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(debug=True)
