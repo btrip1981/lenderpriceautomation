fc5d7f1c0328785c8f40ebe69bb00fde4ceee521
btrip1981 <121128344+btrip1981@users.noreply.github.com>
2023-03-22 10:44:55 -0500

    update to req

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, redirect
 from flask_mail import Mail, Message
 import re
@@ -163,4 +164,4 @@ def send_email(matched_lenders, form_data):
 
 # DO NOT CHANGE ANY OF THIS CODE
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
